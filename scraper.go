87aa6fb4273aa1d28baea200046c0ecee8829bed
hinha <martinuz.dawan9@gmail.com>
2022-06-23 14:45:01 +0700

    enhancement logger trace and request retries

--- scraper.go
+++ scraper.go
@@ -1,14 +1,14 @@
 package sns
 
 import (
-	"errors"
 	"fmt"
 	"io"
-	"log"
 	"math"
 	"net/http"
 	"net/http/cookiejar"
 	"time"
+
+	"github.com/hinha/go-social-network/logger"
 )
 
 type DateRange struct {
@@ -33,6 +33,9 @@ const (
 
 // Config struct contain extra config of twitter
 type Config struct {
+	// Logger
+	Logger logger.Interface
+
 	Date DateRange
 	Lang Lang
 	SearchMode
@@ -61,20 +64,21 @@ func newScraper(conf *Config) *Scraper {
 		},
 	}
 
-	if _, err := time.Parse(dateLayout, conf.Date.Since); err != nil {
-		panic(fmt.Errorf("since %v", err))
-	} else {
-		conf.Date.Since += " 00:00:00"
-	}
+	if conf != nil {
+		if _, err := time.Parse(dateLayout, conf.Date.Since); err != nil {
+			panic(fmt.Errorf("since %v", err))
+		} else {
+			conf.Date.Since += " 00:00:00"
+		}
 
-	if _, err := time.Parse(dateLayout, conf.Date.Until); err != nil {
-		panic(fmt.Errorf("until %v", err))
-	} else {
-		conf.Date.Until += " 23:59:59"
+		if _, err := time.Parse(dateLayout, conf.Date.Until); err != nil {
+			panic(fmt.Errorf("until %v", err))
+		} else {
+			conf.Date.Until += " 23:59:59"
+		}
+		s.conf = conf
 	}
-
 	s.retries = 3
-	s.conf = conf
 
 	return s
 }
@@ -92,53 +96,71 @@ func (c *Scraper) GetClient() *http.Client {
 }
 
 func (c *Scraper) newRequest(method, urls string, paramEncode string, body io.Reader, header http.Header, timeout int, cb callbackResponse) (response *http.Response, err error) {
+	currentLogger, newLogger := c.conf.Logger, logger.Recorder.New()
+
 	c.client.Timeout = time.Duration(timeout) * time.Second
-	//req.URL.RawQuery = paramEncode
 	urls += paramEncode
 	req, err := http.NewRequest(method, urls, body)
 	if err != nil {
 		return nil, err
 	}
+	currentLogger.Init(map[string]interface{}{
+		"subject": "request",
+		"method":  method,
+		"path":    req.URL.Path,
+	})
 
 	req.Header = header
-
+	var redirection []string
 	for i := 1; i < c.retries+1; i++ {
 		response, err = c.client.Do(req)
+		redirection = append(redirection, redirectionUrl(response)) // check redirect url
 		if err != nil {
+			if cb != nil {
+				if len(redirection) != 0 {
+					for i, redirect := range redirection {
+						directLog := fmt.Sprintf("Request %d: %s: %d (Location: %v)", i, redirect, response.StatusCode, response.Header.Get("location"))
+						currentLogger.Debug(newLogger.BeginAt, directLog)
+					}
+				}
+
+				ok, msg := cb(response)
+				if ok {
+					currentLogger.Debug(newLogger.BeginAt, "retrieved successfully", msg)
+					break
+				} else {
+					if i < c.retries {
+						currentLogger.Info(newLogger.BeginAt, "Error retrieving ", req.URL.String(), ", retrying")
+					} else {
+						currentLogger.Error(newLogger.BeginAt, "Error retrieving", req.URL.String())
+					}
+				}
+			}
+
 			if i < c.retries {
 				sleepTime := 1.0 * int(math.Pow(2, float64(i)))
-				log.Printf("Waiting %v seconds", sleepTime)
+				currentLogger.Info(newLogger.BeginAt, fmt.Sprintf("Waiting %v seconds", sleepTime))
 				time.Sleep(time.Duration(sleepTime) * time.Second)
 			}
 
-			if i >= c.retries {
-				msg := fmt.Sprintf("%d request to %s failed, giving up: %v", i, req.URL, err)
-				log.Printf(msg)
-				return nil, errors.New(msg)
-			}
 		}
-
 	}
 
-	if response == nil {
+	currentLogger.Trace(req.Context(), newLogger.BeginAt, func() (string, int) {
+		return "", response.StatusCode
+	}, err)
+
+	if response == nil || err != nil {
+		currentLogger.Error(newLogger.BeginAt, fmt.Sprintf("%d request to %s failed, giving up: %v", c.retries+1, req.URL, err))
 		return nil, fmt.Errorf("reached unreachable code: %v", err)
 	}
 
-	if cb != nil {
-		ok, msg := cb(response)
-		if ok {
-			log.Println(urls, "retrieved successfully", msg)
-		} else {
-			// TODO logging
-			//	if attempt < self._retries:
-			//		retrying = ', retrying'
-			//		level = logging.INFO
-			//	else:
-			//		retrying = ''
-			//		level = logging.ERROR
-			//	logger.log(level, f'Error retrieving {req.url}{msg}{retrying}')
-		}
-	}
-
 	return response, nil
+}
+
+func redirectionUrl(resp *http.Response) string {
+	if resp.StatusCode == http.StatusMovedPermanently {
+		return resp.Request.URL.String()
+	}
+	return ""
 }
